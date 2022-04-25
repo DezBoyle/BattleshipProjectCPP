2d53959aa9b805b621ea073882b92ccfc4e34058
Dez Boyle <dezjames123@gmail.com>
2022-04-25 12:22:44 -0500

    fixed RandomRange

--- Utility.cpp
+++ Utility.cpp
@@ -5,6 +5,6 @@ int Utility::RandomRange(int min, int max)
 {
     //return (min + (rand() % (max - min)));
     static std::mt19937 gen(std::time(nullptr)); //Better to find another proper source of entropy
-    static std::uniform_int_distribution<> dis(min, max-1);
+    std::uniform_int_distribution<> dis(min, max-1);
     return dis(gen);
 }
